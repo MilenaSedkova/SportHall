23fdb71a4b7b6ac8f7dcec0918bf69943cfef05f
MilenaSedkova <sedkovamilena80@gmail.com>
2026-01-18 12:53:52 +0300

    Added models and DTO to AuthMicroService

---
+++ AuthMicroService/DTOs/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthMicroService.DTOs
+{
+    public class LoginDTO
+    {
+        [Required(ErrorMessage = "Введите Email")]
+        [EmailAddress(ErrorMessage = "Некорректный формат Email")]
+        public string email { get; set; } = null!;
+        [Required(ErrorMessage = "Введите пароль")]
+        [MinLength(6,ErrorMessage = "Минимальная длина пароля - 6 символов")]
+        public string password { get; set; } = null!;
+    }
+}
---
+++ AuthMicroService/DTOs/RegistrDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthMicroService.DTOs
+{
+    public class RegistrDTO
+    {
+        [Required(ErrorMessage = "Введите имя пользователя")]
+        public string name { get; set; } = null!;
+        public string? surname { get; set; }
+        [Required(ErrorMessage = "Введите Email")]
+        public string email { get; set; } = null!;
+        [Required(ErrorMessage = "Введите пароль")]
+        public string password { get; set; } = null!;
+        [Required(ErrorMessage = "Введите роль")]
+        public string Role { get; set; } = null!;
+    }
+}
