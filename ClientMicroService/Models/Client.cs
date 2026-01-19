7baf4902e1f1940ced1bdbfa3cfdad843f951848
MilenaSedkova <sedkovamilena80@gmail.com>
2026-01-19 10:31:56 +0300

    Added AuthMicroService

---
+++ ClientMicroService/Models/Client.cs
@@ -0,0 +1,14 @@
+﻿using AuthMicroService.Models;
+
+namespace ClientMicroService.Models
+{
+    public class Client
+    {
+        public string Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Surname { get; set; }
+        public string? Email { get; set; } = null!;
+        public DateOnly DateOfBirth { get; set; }
+        public DateOnly DateOfRegistrationToHall { get; set; }
+    }
+}
