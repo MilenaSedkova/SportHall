8c92c532f2f4c7a079ae9f596ab55f62d2e19112
MilenaSedkova <sedkovamilena80@gmail.com>
2026-01-25 16:31:10 +0300

    replace user configuration to separated file and removed boilerplate config

--- AuthMicroService/ContextDb/AuthContext.cs
+++ AuthMicroService/ContextDb/AuthContext.cs
@@ -6,32 +6,13 @@ namespace AuthMicroService.ContextDb
 {
     public class AuthContext(DbContextOptions<AuthContext> options) : DbContext(options)
     {
-
         public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<User>(entity =>
-            {
-                entity.HasKey(u => u.Id);
-
-                entity.Property(u => u.Name).IsRequired().HasMaxLength(128);
-
-                entity.Property(u => u.Surname).HasMaxLength(128);
-
-                entity.Property(u => u.Email).IsRequired().HasMaxLength(128);
-
-                entity.Property(u => u.PasswordHash).IsRequired();
-
-                entity.Property(u => u.IsActive).IsRequired();
-
-                entity.Property(u => u.Role).HasConversion<string>().IsRequired();
-
-                entity.Property(u => u.RegistratedAt).IsRequired();
-            }
-            );
+            modelBuilder.ApplyConfiguration(new UserConfiguration());         
         }
     }
 }
