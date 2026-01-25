9a21d3a6b31e2079654d74d81ca902cbc49500f9
MilenaSedkova <sedkovamilena80@gmail.com>
2026-01-25 17:27:05 +0300

    Feature/add db context (#6)
    * Added DbContext
    
    * Separted method
    
    * convert to primary constructor
    
    * replace user configuration to separated file and removed boilerplate config
    
    * Update AuthContext

---
+++ AuthMicroService/ContextDb/AuthContext.cs
@@ -0,0 +1,18 @@
+﻿using AuthMicroService.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AuthMicroService.ContextDb
+{
+    public class AuthContext(DbContextOptions<AuthContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AuthContext).Assembly);
+        }
+    }
+}
