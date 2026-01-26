0723cedaed741a6cfc9b518b4d33f7827e1dd949
MilenaSedkova <sedkovamilena80@gmail.com>
2026-01-26 22:04:02 +0300

    added pagination, cancellation tocken, isTracking flag

--- AuthMicroService/Interfaces/IUserRepository.cs
+++ AuthMicroService/Interfaces/IUserRepository.cs
@@ -1,22 +1,22 @@
 ﻿using AuthMicroService.Models;
+using AuthMicroService.PagedResults;
 
-namespace AuthMicroService.Interfaces
+namespace AuthMicroService.Interfaces;
+
+public interface IUserRepository
 {
-    public interface IUserRepository
-    {
-        Task<User?> GetByIdAsyn(Guid id);
+    Task<User?> GetByIdAsynс(Guid id, CancellationToken cancellationToken, bool isTracking);
 
-        Task<User?> GetByEmailAsync(string email);
+    Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken, bool isTracking);
 
-        Task<IEnumerable<User>> GetAllAsync();
+    Task<PagedUserResult<User>> GetAllAsync(int pageNumber, int pageSize, CancellationToken cancellationToken);
 
-        Task<User> CreateUserAsync(User user);
+    Task<User> CreateUserAsync(User user, CancellationToken cancellationTocken);
 
-        Task<bool> UpdateUserAsync(User user);
+    Task<bool> UpdateUserAsync(User user, CancellationToken cancellationToken);
 
-        Task<bool> DeleteUserAsync(Guid id);
+    Task<bool> DeleteUserAsync(Guid id, CancellationToken cancellationToken);
 
-        Task<bool> EmailExistAsync(string email);
-
-    }
+    Task<bool> EmailExistAsync(string email, CancellationToken cancellationToken);
 }
+
---
+++ AuthMicroService/PagedResults/PagedUserResult.cs
@@ -0,0 +1,14 @@
+﻿namespace AuthMicroService.PagedResults
+{
+    public class PagedUserResult<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+
+        public int TotalCount { get; set; } 
+
+        public int PageNumber { get; set; }
+
+        public int PageSize { get; set; }
+
+    }
+}
