5cfe224d06101337748c5a4d5e5fb44174088ab7
MilenaSedkova <sedkovamilena80@gmail.com>
2026-01-26 16:16:48 +0300

    Created IUserRepository

---
+++ AuthMicroService/Interfaces/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using AuthMicroService.Models;
+
+namespace AuthMicroService.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsyn(Guid id);
+
+        Task<User?> GetByEmailAsync(string email);
+
+        Task<IEnumerable<User>> GetAllAsync();
+
+        Task<User> CreateUserAsync(User user);
+
+        Task<bool> UpdateUserAsync(User user);
+
+        Task<bool> DeleteUserAsync(Guid id);
+
+        Task<bool> EmailExistAsync(string email);
+
+    }
+}
