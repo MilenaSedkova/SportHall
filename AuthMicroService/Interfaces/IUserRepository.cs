3a8c0f0d1ed4f0660a06edfb78a294ca4f74e08e
MilenaSedkova <sedkovamilena80@gmail.com>
2026-01-28 20:08:58 +0300

    Created IUserRepository (#8)
    * Created IUserRepository
    
    * added pagination, cancellation tocken, isTracking flag
    
    * Added isTracking to GetAllAsync
    
    * Made class a record
    
    * deleted empty lines between record props

---
+++ AuthMicroService/Interfaces/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using AuthMicroService.Models;
+using AuthMicroService.PagedResults;
+
+namespace AuthMicroService.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User?> GetByIdAsynс(Guid id, bool isTracking, CancellationToken cancellationToken);
+
+    Task<User?> GetByEmailAsync(string email, bool isTracking, CancellationToken cancellationToken);
+
+    Task<PagedUserResult<User>> GetAllAsync(int pageNumber, int pageSize, bool isTracking, CancellationToken cancellationToken);
+
+    Task<User> CreateUserAsync(User user, CancellationToken cancellationTocken);
+
+    Task<bool> UpdateUserAsync(User user, CancellationToken cancellationToken);
+
+    Task<bool> DeleteUserAsync(Guid id, CancellationToken cancellationToken);
+
+    Task<bool> EmailExistAsync(string email, CancellationToken cancellationToken);
+}
+
---
+++ AuthMicroService/PagedResults/PagedUserResult.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthMicroService.PagedResults;
+
+public record PagedUserResult<T>(
+    IEnumerable<T> Items,
+    int TotalCount,
+    int PageNumber,
+    int PageSize
+);
+
