5beba0cf88fa0b1969042cf20eb6be3a4e6eb998
MilenaSedkova <sedkovamilena80@gmail.com>
2026-02-02 13:25:07 +0300

    Added Error messages

--- AuthMicroService/DTOs/UpdateUserDTO.cs
+++ AuthMicroService/DTOs/UpdateUserDTO.cs
@@ -8,15 +8,17 @@ namespace AuthMicroService.DTOs
     {
         public Guid Id { get; set; }
 
-        [MaxLength(UserEntityConstants.MaxNameLength)]
+        [MaxLength(UserEntityConstants.MaxNameLength)]    
         public string? Name { get; set; }
 
         [MaxLength(UserEntityConstants.MaxSurnameLength)]
         public string? Surname { get; set; }
 
         [MaxLength(UserEntityConstants.MaxEmailLength)]
+        [EmailAddress(ErrorMessage = "Email has incorrect format")]
         public string? Email { get; set; }
 
+        [EnumDataType(typeof(UserRole), ErrorMessage = "Invalid role")]
         public UserRole? Role { get; set; }
 
         public bool? IsActive { get; set; }
