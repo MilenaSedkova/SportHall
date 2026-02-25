0765d99c7cb9b8ea38aedb4b73f86c4ff33e5fea
MilenaSedkova <sedkovamilena80@gmail.com>
2026-02-25 18:35:26 +0300

    Add global exception handling middleware

---
+++ AuthService/AuthService.API/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using AuthService.Business.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace AuthService.API.Middleware;
+
+public class ExceptionHandlingMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch(Exception ex) 
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception ex)
+    {
+        var statusCode = ex switch
+        {
+            NotFoundException => (int)HttpStatusCode.NotFound,
+            UnauthorizedAccessException => (int)HttpStatusCode.Unauthorized,
+            _ => (int)HttpStatusCode.InternalServerError
+        };
+
+        var response = new
+        {
+            error = ex.Message,
+            status = statusCode
+        };
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = statusCode;
+
+        return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+    }
+}
--- AuthService/AuthService.DataAccess/ContextDb/UserConfiguration.cs
+++ AuthService/AuthService.DataAccess/ContextDb/UserConfiguration.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using AuthService.DataAccess.Models;
-using System.Reflection.Emit;
 
 namespace AuthMicroService.ContextDb;
 
