aecb28a932f9f23ff16173c424d192b0e2557125
MilenaSedkova <sedkovamilena80@gmail.com>
2026-03-20 15:12:13 +0300

    Add DTOs (#19)
    * Add DTOs
    
    * remove empty line

---
+++ ClientService/ClientService.Business/DTOs/UpdateDto.cs
@@ -0,0 +1,24 @@
+﻿using ClientService.DataAccess.Enums;
+using ClientService.DataAccess.Validation;
+using System.ComponentModel.DataAnnotations;
+using ClientService.DataAccess.Constants;
+
+namespace ClientService.Business.DTOs;
+
+public class UpdateDto
+{
+    [MaxLength(ClientConstants.MaxPhoneNumberLength, ErrorMessage = "The length of the entered phone number exceeds the maximum")]
+    [BelarusPhoneNumber(ErrorMessage = "The number format is incorrect")]
+    public string? PhoneNumber { get; set; }
+
+    public DateOnly? BirthDate { get; set; }
+
+    public ClientGender? Gender { get; set; }
+
+    [MaxLength(ClientConstants.MaxNameLength, ErrorMessage = "The length of enetered name exceeds the maximum")]
+    public string? EmergencyContactName { get; set; }
+
+    [MaxLength(ClientConstants.MaxPhoneNumberLength, ErrorMessage = "The length of entered phone number exceeds the maximum")]
+    [BelarusPhoneNumber(ErrorMessage = "The number format is incorrect")]
+    public string? EmergencyContactPhone { get; set; }
+}
