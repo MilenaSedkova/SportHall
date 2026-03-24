9576ff905e23b7dcbbd3bf5bfc2ef602e74803f2
MilenaSedkova <sedkovamilena80@gmail.com>
2026-03-24 11:39:31 +0300

    Add IClientRepository

---
+++ ClientService/ClientService.DataAccess/Interfaces/IClientRepository.cs
@@ -0,0 +1,25 @@
+﻿using ClientService.DataAccess.Enums;
+using ClientService.DataAccess.Models;
+using ClientService.DataAccess.PagedResults;
+
+namespace ClientService.DataAccess.Interfaces;
+public interface IClientRepository
+{
+    Task<Client?> GetClientById(Guid id, bool isTracking, CancellationToken cancellationToken);
+
+    Task<Client?> GetClientByUserIdAsync(Guid userId, bool isTracking, CancellationToken cancellationToken);
+
+    Task<Client?> GetClientByEmailAsync(string email, bool isTracking, CancellationToken cancellationToken);
+
+    Task<PagedClientResult<Client>> GetAllAsync(int pageSize, int pageNumber, bool isTracking, CancellationToken cancellationToken);
+
+    Task<PagedClientResult<Client>> GetClientByGenderAsync(int pageSize, int pageNumber, ClientGender gender, bool isTracking, CancellationToken cancellationToken);
+
+    Task<PagedClientResult<Client>> GetClientByPhoneNumberAsync(int pageSize, int pageNumber, string phoneNumber, bool isTracking, CancellationToken cancellationToken);
+
+    Task<Client> CreateClientAsync(Client client, CancellationToken cancellationToken);
+
+    Task<Client> UpdateClientAsync(Client client, CancellationToken cancelToken);
+
+    Task<Client> DeleteClientAsync(Guid id, CancellationToken cancellationToken);
+}
