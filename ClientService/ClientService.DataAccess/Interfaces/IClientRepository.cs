e48dd05c0094beca35a98df37758daf2e713c80e
MilenaSedkova <sedkovamilena80@gmail.com>
2026-03-25 18:26:44 +0300

    Add ClientRepository

--- ClientService/ClientService.DataAccess/Interfaces/IClientRepository.cs
+++ ClientService/ClientService.DataAccess/Interfaces/IClientRepository.cs
@@ -5,21 +5,15 @@ using ClientService.DataAccess.PagedResults;
 namespace ClientService.DataAccess.Interfaces;
 public interface IClientRepository
 {
-    Task<Client?> GetClientById(Guid id, bool isTracking, CancellationToken cancellationToken);
-
-    Task<Client?> GetClientByUserIdAsync(Guid userId, bool isTracking, CancellationToken cancellationToken);
-
-    Task<Client?> GetClientByEmailAsync(string email, bool isTracking, CancellationToken cancellationToken);
+    Task<Client?> GetClientByIdAsync(Guid id, bool isTracking, CancellationToken cancellationToken);
 
     Task<PagedResult<Client>> GetAllAsync(int pageSize, int pageNumber, bool isTracking, CancellationToken cancellationToken);
 
     Task<PagedResult<Client>> GetClientByGenderAsync(int pageSize, int pageNumber, ClientGender gender, bool isTracking, CancellationToken cancellationToken);
 
-    Task<PagedResult<Client>> GetClientByPhoneNumberAsync(int pageSize, int pageNumber, string phoneNumber, bool isTracking, CancellationToken cancellationToken);
-
     Task<Client> CreateClientAsync(Client client, CancellationToken cancellationToken);
 
-    Task<Client> UpdateClientAsync(Client client, CancellationToken cancelToken);
+    Task<bool> UpdateClientAsync(Client client, CancellationToken cancelToken);
 
-    Task<Client> DeleteClientAsync(Guid id, CancellationToken cancellationToken);
+    Task<bool> DeleteClientAsync(Guid id, CancellationToken cancellationToken);
 }
