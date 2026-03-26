82e6b442cb77962f32fdcffec262aceab609d90a
MilenaSedkova <sedkovamilena80@gmail.com>
2026-03-26 15:44:57 +0300

    Add ClientRepository (#22)
    * Add ClientRepository
    
    * Add ExecuteDeleteAsync

---
+++ ClientService/ClientService.DataAccess/Repository/ClientRepository.cs
@@ -0,0 +1,75 @@
+﻿using ClientService.DataAccess.ContextDb;
+using ClientService.DataAccess.Interfaces;
+using ClientService.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using ClientService.DataAccess.PagedResults;
+using ClientService.DataAccess.Enums;
+
+namespace ClientService.DataAccess.Repository;
+
+public class ClientRepository(ClientContext clientSet) : IClientRepository
+{
+    public async Task<Client?> GetClientByIdAsync(Guid id, bool isTracking, CancellationToken cancellationToken)
+    {
+        var query = isTracking ? clientSet.Clients.AsQueryable() : clientSet.Clients.AsNoTracking();
+
+        return await query.FirstOrDefaultAsync(c => c.Id == id, cancellationToken);
+    }
+    
+    public async Task<PagedResult<Client>> GetAllAsync(int pageSize, int pageNumber, bool isTracking, CancellationToken cancellationToken)
+    {
+        var query = isTracking ? clientSet.Clients.AsQueryable() : clientSet.Clients.AsNoTracking();
+
+        var totalCount = await query.CountAsync(cancellationToken);
+
+        var items = await query
+            .OrderBy(c => c.Id)
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize) 
+            .ToListAsync();
+
+        return new PagedResult<Client>(items, pageSize, pageNumber, totalCount);
+    }
+
+    public async Task<PagedResult<Client>> GetClientByGenderAsync(int pageSize, int pageNumber, ClientGender gender, bool isTracking, CancellationToken cancellationToken)
+    {
+        var query = isTracking ? clientSet.Clients.AsQueryable() : clientSet.Clients.AsNoTracking();
+
+        query = query.Where(c => c.Gender == gender);
+
+        var totalCount = await query.CountAsync(cancellationToken);
+
+        var items = await query
+            .OrderBy(c => c.Id)
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+
+        return new PagedResult<Client>(items, pageSize, pageNumber, totalCount);
+    }
+
+    public async Task<Client> CreateClientAsync(Client client, CancellationToken cancellationToken)
+    {
+        await clientSet.Clients.AddAsync(client);
+        await clientSet.SaveChangesAsync();
+
+        return client;
+    }
+
+    public async Task<bool> UpdateClientAsync(Client client, CancellationToken cancellationToken)
+    {
+        clientSet.Clients.Update(client);
+        var changedRows = await clientSet.SaveChangesAsync();
+
+        return changedRows > 0;
+    }
+
+    public async Task<bool> DeleteClientAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var deletedCount = await clientSet.Clients
+            .Where(c => c.Id == id)
+            .ExecuteDeleteAsync();
+
+        return deletedCount > 0;
+    }
+}
