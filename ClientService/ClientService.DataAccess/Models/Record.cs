cbcbacf5adc9362fd2d341b912d7d89a8e3e6b4c
MilenaSedkova <sedkovamilena80@gmail.com>
2026-09-18 15:20:45 +0300

    Add Coach and Record models

---
+++ ClientService/ClientService.DataAccess/Models/Record.cs
@@ -0,0 +1,18 @@
+﻿namespace ClientService.DataAccess.Models;
+
+public class Record
+{
+    public Guid Id { get; set; }
+
+    public Guid ClientId { get; set; }
+
+    public Client Client { get; set; } = null!;
+
+    public Guid CoachId { get; set; }
+
+    public Coach Coach { get; set; } = null!;
+
+    public DateTime StartTime { get; set; }
+
+    public DateTime EndTime { get; set; }
+}
