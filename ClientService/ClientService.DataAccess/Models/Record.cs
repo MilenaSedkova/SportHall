a4ce742a5b88a23d8d4ef24f7d423a6ff7cf40d1
MilenaSedkova <sedkovamilena80@gmail.com>
2026-09-20 14:57:27 +0300

    change time type to DateTimeOffset

--- ClientService/ClientService.DataAccess/Models/Record.cs
+++ ClientService/ClientService.DataAccess/Models/Record.cs
@@ -12,7 +12,7 @@ public class Record
 
     public Coach Coach { get; set; } = null!;
 
-    public DateTime StartTime { get; set; }
+    public DateTimeOffset StartTime { get; set; }
 
-    public DateTime EndTime { get; set; }
+    public DateTimeOffset EndTime { get; set; }
 }
